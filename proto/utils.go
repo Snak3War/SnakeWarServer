2613aded4b93595fd616886562cbcbeb8973f855
agfn <lavender.tree9988@gmail.com>
2021-10-23 12:16:19 +0800

    feat: use LittleEndian

--- proto/utils.go
+++ proto/utils.go
@@ -4,12 +4,12 @@ import "encoding/binary"
 
 func AppendUint32(data []byte, v uint32) []byte {
 	var buf [4]byte
-	binary.BigEndian.PutUint32(buf[:], v)
+	binary.LittleEndian.PutUint32(buf[:], v)
 	return append(data, buf[:]...)
 }
 
 func AppendUint16(data []byte, v uint16) []byte {
 	var buf [2]byte
-	binary.BigEndian.PutUint16(buf[:], v)
+	binary.LittleEndian.PutUint16(buf[:], v)
 	return append(data, buf[:]...)
 }
